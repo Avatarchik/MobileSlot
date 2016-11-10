e3ab46a22e47b1a347a585a01b645a9d29c4f197
druggedCode <druggedcode@gmail.com>
2016-11-11 02:17:31 +0900

    Locate nested class
    - 스핀 응답 내부적으로 사용하는 클래스는 중첩 클래스로 수정

--- Assets/4_Scripts/net/GameDTO.cs
+++ Assets/4_Scripts/net/GameDTO.cs
@@ -1,6 +1,4 @@
-﻿//todo
-//class 가 아니라 struct 도 고려
-public class DTO
+﻿public class DTO
 {
 
 }
@@ -19,43 +17,13 @@ public class ResDTO
         public int last_line_bet { get; set; }
         public int max_line_bet { get; set; }
 
-        public double? jackpotPool{get;set;}
+        public double? jackpotPool { get; set; }
     }
 
     public class Spin : DTO
     {
-        public class Payout
-        {
-            public float lineBet{get;set;}
-            public double totalPayout{get;set;}
-            public float multipleWin{get;set;}
-            public bool isMegaWin{get;set;}
-            public bool isBigWin{get;set;}
-            public bool isJackpot{get;set;}
-            
-            public SpinInfo[] spins{get;set;}
-        }
-
-        public class SpinInfo
-        {
-            public string[] reel{get;set;} //결과 심볼들
-            public int freeSpinCount { get;set;}
-            public double totalPayout{get;set;}
-            public bool isNormal{get;set;}
-            public int[] fixedreel{get;set;}
-            public Payline[] payLines{get;set;}
-        }
-        
-        public class Payline
-        {
-            public double payout{get;set;}
-            public int matches{get;set;}
-            public int line{get;set;}
-            public bool isJackpot{get;set;}
-        }
-
-        public int levelPercent{ get; set; }
-        public int level{ get; set; }
+        public int levelPercent { get; set; }
+        public int level { get; set; }
         public int gameLevel { get; set; }
         public int gameLevelPercent { get; set; }
         public double balance { get; set; }
@@ -63,11 +31,43 @@ public class ResDTO
         public double level_up_bonus { get; set; }
 
 
-        public string freeSpinKey{get;set;}
-        public double? jackpotPool{get;set;}
-        public int winID{get;set;}
+        public string freeSpinKey { get; set; }
+        public double? jackpotPool { get; set; }
+        public int winID { get; set; }
 
-        public Payout payouts{get;set;}
+        public Payout payouts { get; set; }
+
+        public class Payout
+        {
+            public float lineBet { get; set; }
+            public double totalPayout { get; set; }
+            public float multipleWin { get; set; }
+            public bool isMegaWin { get; set; }
+            public bool isBigWin { get; set; }
+            public bool isJackpot { get; set; }
+
+            public SpinInfo[] spins{get;set;}
+
+            // public SpinInfo[] spins { get; set; }
+
+            public class SpinInfo
+            {
+                public string[] reel { get; set; } //결과 심볼들
+                public int freeSpinCount { get; set; }
+                public double totalPayout { get; set; }
+                public bool isNormal { get; set; }
+                public int[] fixedreel { get; set; }
+                public Payline[] payLines { get; set; }
+
+                public class Payline
+                {
+                    public double payout { get; set; }
+                    public int matches { get; set; }
+                    public int line { get; set; }
+                    public bool isJackpot { get; set; }
+                }
+            }
+        }
     }
 }
 
