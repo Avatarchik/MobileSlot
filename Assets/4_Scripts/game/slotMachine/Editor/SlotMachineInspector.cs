3cb9516d71348920e50138977666fac0a6e5e3a5
druggedcode <druggedcode@gmail.com>
2016-11-17 18:43:50 +0900

    Create editor class 'SlotMachineInspector'

---
+++ Assets/4_Scripts/game/slotMachine/Editor/SlotMachineInspector.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEditor;
+
+[CustomEditor(typeof(SlotMachine))]
+public class SlotMachineInspector : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		// DrawDefaultInspector();
+	}
+}
