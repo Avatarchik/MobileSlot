44aad5b49d3eccae469401c570484c9076fd61de
druggedCode <druggedcode@gmail.com>
2016-11-20 18:06:18 +0900

    Rename 'SlotUI' to 'SlotMachineUI'

---
+++ Assets/4_Scripts/game/slotMachine/ui/SlotMachineUI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SlotMachineUI : MonoBehaviour
+{
+    public Text TxtBalance;
+    void Awake()
+    {
+        CanvasUtil.CanvasSetting(GetComponent<Canvas>());
+    }
+
+    void Start()
+    {
+        SetBalance(0);
+    }
+
+    public void Idle()
+    {
+		UpdateBalance();
+    }
+
+    public void UpdateBalance()
+    {
+        SetBalance(SlotModel.Instance.Owner.Balance);
+    }
+
+    void SetBalance(double balance)
+    {
+        TxtBalance.text = balance.ToString("#,##0");
+    }
+}
