62a263ed6aa6d84a28fbf4900ffffdda82f224cb
druggedCode <druggedcode@gmail.com>
2016-11-20 18:03:54 +0900

    Create class 'User'

---
+++ Assets/4_Scripts/User.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class User
+{
+    public int Level { get; set; }
+    public int LevelPercent { get; set; }
+    public int GameLevel { get; set; }
+    public int GameLevelPercent { get; set; }
+    public double Balance { get; set; }
+
+    public void Update(ResDTO dto)
+    {
+        Balance = dto.balance;
+        Level = dto.level;
+        LevelPercent = dto.levelPercent;
+        GameLevel = dto.gameLevel;
+        GameLevelPercent = dto.gameLevelPercent;
+    }
+
+    public void Reset()
+    {
+        Balance = 0;
+    }
+}
