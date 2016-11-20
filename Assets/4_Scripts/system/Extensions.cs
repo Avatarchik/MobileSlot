f1a7915d83181c17726362b15ad0751f2184439e
druggedCode <druggedcode@gmail.com>
2016-11-21 00:32:19 +0900

    Add extension that slice array

---
+++ Assets/4_Scripts/system/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public static class ExtensionMethods
+{
+    public static T[] Slice<T>(this T[] arr, int indexFrom, int indexTo)
+    {
+        if (indexFrom > indexTo)
+        {
+            throw new ArgumentOutOfRangeException("indexFrom is bigger than indexTo!");
+        }
+
+        int length = indexTo - indexFrom;
+        T[] result = new T[length];
+        Array.Copy(arr, indexFrom, result, 0, length);
+
+        return result;
+    }
+}
