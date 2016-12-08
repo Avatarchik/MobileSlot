2b430dd0434d90a027e8eb08186689b5ab16e84b
druggedcode <druggedcode@gmail.com>
2016-12-08 18:15:58 +0900

    Add class 'FreeSpinDirector'

---
+++ Assets/4_Scripts/game/slotMachine/FreeSpinDirector.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class FreeSpinDirector : MonoBehaviour
+{
+    [SerializeField]
+    protected float _limitTime = 15f;
+
+    protected float _openendTime;
+
+    public int? SelectedKind { get; protected set; }
+
+    virtual public IEnumerator FreeSpinReady()
+    {
+        yield break;
+    }
+
+    virtual public IEnumerator Trigger()
+    {
+        yield break;
+    }
+
+    virtual public IEnumerator Retrigger()
+    {
+        yield break;
+    }
+
+    virtual public IEnumerator Select()
+    {
+        yield break;
+    }
+
+    protected float ElapsedTime
+    {
+        get { return Time.time - _openendTime; }
+    }
+}
---
+++ Assets/Resources/games/02/scripts/HighDiamondsFreeSpinDirector.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class HighDiamondsFreeSpinDirector : FreeSpinDirector
+{
+    public GameObject selector;
+    public Button spin30;
+    public Button spin15;
+    public Button spin10;
+
+
+
+    void Awake()
+    {
+        spin30.onClick.AddListener(() => OnClick(1));
+        spin15.onClick.AddListener(() => OnClick(2));
+        spin10.onClick.AddListener(() => OnClick(3));
+
+        selector.gameObject.SetActive(false);
+    }
+
+
+    void OnClick(int index)
+    {
+        Debug.Log(" click: " + index);
+        SelectedKind = index;
+    }
+
+
+    override public IEnumerator Select()
+    {
+        _openendTime = Time.time;
+
+        selector.gameObject.SetActive(true);
+
+        while (SelectedKind == null && ElapsedTime < _limitTime)
+        {
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        if (SelectedKind == null) SelectedKind = Random.Range(0, 3) + 1;
+
+        yield break;
+    }
+}
