ea3150400dcb24c27d96be08c1df3c3dddcbf024
druggedcode <druggedcode@gmail.com>
2016-12-13 16:41:51 +0900

    Add class 'SoundPlayer'

---
+++ utils/GameObjectUtil.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace lpesign
+{
+    public class GameObjectUtil
+    {
+
+        static public GameObject Create(string name, Transform parent = null)
+        {
+            GameObject go = new GameObject(name);
+
+            if (parent != null)
+            {
+                go.transform.SetParent(parent);
+            }
+
+            return go;
+        }
+
+        static public T Create<T>(string name, Transform parent = null) where T : Component
+        {
+            return Create(name, parent).AddComponent<T>();
+        }
+
+        static public Transform CreateEmptyContainer(string name, Transform parent = null)
+        {
+            return Create(name, parent).transform;
+        }
+    }
+}
+
