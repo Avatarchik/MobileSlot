5ad4e71f5659577acf9cc6b3717213e8dd7a69ce
druggedcode <druggedcode@gmail.com>
2016-12-13 16:43:49 +0900

    Use 'SoundPlayer' in 'SoundManager'

--- Assets/4_Scripts/system/SoundManager.cs
+++ Assets/4_Scripts/system/SoundManager.cs
@@ -3,8 +3,16 @@ using System.Collections;
 
 using lpesign;
 
-public class SoundManager : Singleton<SoundManager>
+[RequireComponent(typeof( SoundPlayer ))]
+public class SoundManager : SingletonSimple<SoundManager>
 {
+	SoundPlayer _player;
+	override protected void Awake()
+	{
+		base.Awake();
+		_player = GetComponent<SoundPlayer>();
+	}
+
 	public void PlayFreeSpinTrigger()
 	{
 
