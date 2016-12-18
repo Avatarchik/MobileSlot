1484e7f8fe8bda9626968a9fd85401d9e04136fd
druggedCode <druggedcode@gmail.com>
2016-12-18 23:18:05 +0900

    Ongoing SoundList Editor

---
+++ Editor/UsableEditor.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+
+namespace lpesign.UnityEditor
+{
+
+    public class UsableEditor<T> : Editor where T : MonoBehaviour
+    {
+        protected T _script;
+        protected MonoScript _mono;
+
+        protected void FindScript()
+        {
+            _script = (T)target;
+        }
+
+        protected void DrawScript()
+        {
+            if (_script == null) FindScript();
+
+            GUI.enabled = false;
+            _mono = EditorGUILayout.ObjectField("Script", MonoScript.FromMonoBehaviour(_script), typeof(T), false) as MonoScript;
+            GUI.enabled = true;
+            EditorGUILayout.Space();
+        }
+
+        protected void DrawRowLine(string lineName = "")
+        {
+            EditorGUILayout.Space();
+            GUI.color = Color.white;
+            GUILayout.Box("", new GUILayoutOption[] { GUILayout.ExpandWidth(true), GUILayout.Height(1) });
+            if (string.IsNullOrEmpty(lineName) == false) GUILayout.Label(lineName, EditorStyles.boldLabel);
+        }
+
+        // 1. public ReorderableList(IList elements, Type elementType),
+        // 2. public ReorderableList(SerializedObject serializedObject, SerializedProperty elements).
+        protected ReorderableList CreateReorderableList(string propertyName,
+                                                        string HeaderName = "",
+                                                        bool draggable = true,
+                                                        bool displayAddButton = true,
+                                                        bool displayRemoveButton = true)
+        {
+            SerializedProperty property = serializedObject.FindProperty(propertyName);
+            return CreateReorderableList(property, HeaderName);
+        }
+
+        protected ReorderableList CreateReorderableList(SerializedProperty property,
+                                                        string HeaderName = "",
+                                                        bool draggable = true,
+                                                        bool displayAddButton = true,
+                                                        bool displayRemoveButton = true)
+        {
+            bool displayHeader = !string.IsNullOrEmpty(HeaderName);
+
+            var list = new ReorderableList(serializedObject, property, draggable, displayHeader, displayAddButton, displayRemoveButton);
+            if (displayHeader)
+            {
+                list.drawHeaderCallback = (Rect rect) =>
+                {
+                    GUI.color = Color.yellow;
+                    EditorGUI.LabelField(rect, HeaderName, EditorStyles.boldLabel);
+                    GUI.color = Color.white;
+                };
+            }
+            return list;
+        }
+    }
+}
---
+++ sound/SoundList.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace lpesign
+{
+    [Serializable]
+    public class SoundSchema
+    {
+        public string name;
+        public AudioClip clip;
+    }
+
+    [Serializable]
+    public class SoundGroup
+    {
+        public enum PlayType
+        {
+            RANDOM,
+            Loop
+        }
+
+        public string name;
+        public PlayType type;
+        public List<SoundSchema> sounds;
+    }
+
+    [Serializable]
+    public class SoundList : MonoBehaviour
+    {
+        public SoundSchema[] basic;
+        public SoundGroup[] categories;
+    }
+}
