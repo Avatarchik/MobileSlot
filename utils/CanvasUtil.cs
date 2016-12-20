622325cd06510d52ef234b92a3809955eb37ff06
druggedcode <druggedcode@gmail.com>
2016-12-20 18:11:25 +0900

    Update UtilExtension

--- utils/CanvasUtil.cs
+++ utils/CanvasUtil.cs
@@ -5,14 +5,15 @@ namespace lpesign
 {
     public class CanvasUtil
     {
-        static public void CanvasSetting(Canvas canvas, float referenceWidth, float referenceHeight, float pixelPerUnit = 100f)
-        {
-            if (canvas == null) return;
+    }
 
+    static public class CanvasExtension
+    {
+        static public void SetReferenceSize(this Canvas canvas, float referenceWidth, float referenceHeight, float pixelPerUnit = 100f)
+        {
             Camera cam = canvas.worldCamera;
             if (cam == null) return;
 
-            // cam.orthographicSize = GlobalConfig.ReferenceHeight * 0.5f / GlobalConfig.PixelPerUnit;
             cam.orthographicSize = referenceHeight * 0.5f / pixelPerUnit;
 
             CanvasScaler canvasScaler = canvas.GetComponent<CanvasScaler>();
@@ -20,7 +21,6 @@ namespace lpesign
             {
                 canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
                 canvasScaler.matchWidthOrHeight = 1f;
-                // canvasScaler.referenceResolution = new Vector2(GlobalConfig.ReferenceWidth, GlobalConfig.ReferenceHeight);
                 canvasScaler.referenceResolution = new Vector2(referenceWidth, referenceHeight);
             }
         }
